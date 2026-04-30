0eae76573e9ff14395560901a46e5f2bb2ee2d67
Zabedfolio <zabedfolio@gmail.com>
2026-04-30 18:13:36 +0600

    added card in all tiles page

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import TileCard from "@/components/TileCard";
 
-const AllTilesPage = () => {
+const AllTilesPage = async() => {
+
+    const res = await fetch('https://tiles-gallery-zabedfolio.vercel.app/data/products.json')
+    const data = await res.json();
+    // console.log(data,"data")
+
     return (
-        <div>
-            This is all tiles page
+        <div className='container mx-auto mt-10'>
+            <h1 className="text-center font-bold text-4xl">All Tiles</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 container mx-auto mt-10'>
+                {
+                    data.map(tile =>
+                        <TileCard key={tile.id} tile={tile}></TileCard>
+                    )
+                }
+            </div>
         </div>
     );
 };
