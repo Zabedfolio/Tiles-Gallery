4015ecd7540db99d2c58aef9a2092d25992ec41b
Zabedfolio <zabedfolio@gmail.com>
2026-04-30 19:08:05 +0600

    added dynamic search bar

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -1,30 +1,29 @@
+import Search from "@/components/Search";
 import TileCard from "@/components/TileCard";
 
-const AllTilesPage = async () => {
+const AllTilesPage = async ({ searchParams }) => {
 
-    const res = await fetch('https://tiles-gallery-zabedfolio.vercel.app/data/products.json')
+    const res = await fetch('https://tiles-gallery-zabedfolio.vercel.app/data/products.json');
     const data = await res.json();
-    // console.log(data,"data")
+
+    const params = await searchParams;
+    const search = params?.search || "";
+
+    const filtered = data.filter(tile =>
+        tile.title.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <div className='container mx-auto mt-10'>
 
-
-            <div className="sticky top-0 z-50 bg-white">
-                <div className="container mx-auto px-4 py-4">
-                    <input
-                        type="text"
-                        placeholder="Search tiles..."
-                        className="w-full px-5 py-3 rounded-xl border border-gray-200 focus:outline-none focus:ring-2 focus:ring-black transition"
-                    />
-                </div>
-            </div>
-
             <h1 className="text-center font-bold text-4xl">All Tiles</h1>
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 container mx-auto mt-10'>
+
+            <Search />
+
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-10'>
                 {
-                    data.map(tile =>
-                        <TileCard key={tile.id} tile={tile}></TileCard>
+                    filtered.map(tile =>
+                        <TileCard key={tile.id} tile={tile} />
                     )
                 }
             </div>
