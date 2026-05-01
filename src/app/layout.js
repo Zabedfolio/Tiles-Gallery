b719f53f9b167c529575ff183f20a513a9cdc405
Zabedfolio <zabedfolio@gmail.com>
2026-05-01 23:01:06 +0600

    added signup form and work function

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,10 @@ export default function RootLayout({ children }) {
         <NavBar></NavBar>
         <main>
           {children}
+          <ToastContainer position="top-right" 
+          style={{ top: "80px" }} 
+          toastStyle={{ width: "400px" }}
+          ></ToastContainer>
         </main>
         <Footer></Footer>
         </body>
